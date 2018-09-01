145b21746e5b54cd8d711ca71da211b779e8b22a
Nikita Nikitin <nioktn@gmail.com>
2018-09-01 08:50:27 +0300

    Repo initialized, project created

---
+++ KlavTest/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace KlavTest
+{
+    [TestFixture]
+    class BaseTest
+    {
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+        public virtual void FirstInitialize()
+        {
+
+        }
+
+    }
+}
---
+++ KlavTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KlavTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KlavTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d58e939-6094-4dd2-8863-23f599a45955")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
